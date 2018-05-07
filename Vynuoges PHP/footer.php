ff7e93c81c36054315f08b53512ef4b171f52529
grafundzijus <grafas81@gmail.com>
2018-05-07 12:08:37 +0300

    vynuogės php bandymas

---
+++ Vynuoges PHP/footer.php
@@ -0,0 +1,30 @@
+<footer class="page-footer purple lighten-3">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Kontaktai</h5>
+          <p class="grey-text text-lighten-4">IĮ „Ekoflora“</p>
+          <p class="grey-text text-lighten-4">Andrius Diliautas</p>
+          <p class="grey-text text-lighten-4">El. paštas: INFO@VYNUOGES.LT</p>
+          <p class="grey-text text-lighten-4"><button data-target="modal1" class="btn modal-trigger">Rašykite mums</button></p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text"></h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="index.html">Pagrindinis</a></li>
+            <li><a class="grey-text text-lighten-3" href="about.html">Apie</a></li>
+            <li><a class="grey-text text-lighten-3" href="#">Patarimai</a></li>
+            <li><a class="grey-text text-lighten-3" href="galery.html"> Galerija</a></li>
+            <li><a class="grey-text text-lighten-3" href="#">Parduotuvė</a></li>
+            <li> <button onclick="document.getElementById('id01').style.display='block'">Prisijungti</button></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      © 2018 Copyright
+      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
---
+++ Vynuoges PHP/header.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Pagrindinis</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="css/master.css">
+
+
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+<nav>
+  <div class="nav-wrapper purple lighten-3">
+    <a href="#!" class="brand-logo"><img src="img/vynuoges-logo-214x67.png" alt=""> </a>
+    <a href="#!" data-target="mobile-demo" class="sidenav-trigger"><img src="img/vynuoges-logo-214x67.png" alt=""><i class="material-icons"></i></a>
+    <ul class="right hide-on-med-and-down">
+      <li> <a href="index.html">Pagrindinis</a></li>
+      <li> <a href="about.html">Apie</a></li>
+      <li> <a href="#">Patarimai</a></li>
+      <li> <a href="galery.html"> Galerija</a>  </li>
+      <li> <a href="#">Parduotuvė</a></li>
+      <li> <button onclick="document.getElementById('id01').style.display='block'">Prisijungti</button></li>
+    </ul>
+  </div>
+</nav>
+
+<ul class="sidenav" id="mobile-demo">
+  <li> <a href="index.html">Pagrindinis</a></li>
+  <li> <a href="about.html">Apie</a></li>
+  <li> <a href="#">Patarimai</a></li>
+  <li> <a href="galery.html"> Galerija</a></li>
+  <li> <a href="#">Parduotuvė</a></li>
+  <li> <button onclick="document.getElementById('id01').style.display='block'">Prisijungti</button></li>
+</ul>
